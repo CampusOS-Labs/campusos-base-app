530e5fe2e66a608c0e74c43dc9dadc528c03f16f
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-06-15 14:41:08 +0530

    fix teacher name

--- lib/config/teachers.ts
+++ lib/config/teachers.ts
@@ -4,7 +4,7 @@ export const TEACHERS = [
   { id: "t2", name: "pooja chavan", phone: "918698037718" },
   { id: "t3", name: "diksha dhande", phone: "919834595629" },
   { id: "t4", name: "vaishnavi andekar", phone: "917263932554" },
-  { id: "t5", name: "mitali pungaliy", phone: "918847728549" },
+  { id: "t5", name: "mitali pungaliya", phone: "918847728549" },
   { id: "t6", name: "amaan bilwar", phone: "15137997001" },
   { id: "t7", name: "samarth ghadipatil", phone: "917038667755" },
 ] as const;
